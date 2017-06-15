9923154ad24315ea87d78699e1bf286c2d12ecc1
damienbod <damien_bod@hotmail.com>
2017-06-15 09:48:29 +0200

    init application

---
+++ src/AngularClient/angularApp/app/auth/modules/auth.configuration.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthConfiguration {
+
+    stsServer = 'https://accounts.google.com';
+
+    redirect_url = 'https://localhost:44386';
+
+    // The Client MUST validate that the aud (audience) Claim contains its client_id value registered at the Issuer identified by the iss (issuer) Claim as an audience.
+    // The ID Token MUST be rejected if the ID Token does not list the Client as a valid audience, or if it contains additional audiences not trusted by the Client.
+    client_id = '188968487735-b1hh7k87nkkh6vv84548sinju2kpr7gn.apps.googleusercontent.com';
+
+    response_type = 'id_token token';
+
+    scope = 'openid email profile';
+
+    post_logout_redirect_uri = 'https://localhost:44386/Unauthorized';
+
+    start_checksession = false;
+
+    silent_renew = true;
+
+    startup_route = '/home';
+
+    // HTTP 403
+    forbidden_route = '/Forbidden';
+
+    // HTTP 401
+    unauthorized_route = '/Unauthorized';
+
+    log_console_warning_active = true;
+
+    log_console_debug_active = true;
+
+    // id_token C8: The iat Claim can be used to reject tokens that were issued too far away from the current time,
+    // limiting the amount of time that nonces need to be stored to prevent attacks.The acceptable range is Client specific.
+    max_id_token_iat_offset_allowed_in_seconds = 3;
+
+    override_well_known_configuration = true;
+
+    override_well_known_configuration_url = 'https://localhost:44386/wellknownconfiguration.json';
+}
