ff104550f598f1128b67901f116bddec2bf2e290
damienbod <damien_bod@hotmail.com>
2017-08-11 10:47:17 +0200

    updating to version 1.3.6

--- src/AngularClient/angularApp/app/app.module.ts
+++ src/AngularClient/angularApp/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Router } from '@angular/router';
 import { AppComponent } from './app.component';
 import { Configuration } from './app.constants';
 import { routing } from './app.routes';
