2bdac468b747893516a6a8a8dc7bb2b55ad1e52d
damienbod <damien_bod@hotmail.com>
2020-05-03 07:56:24 +0200

    updating angular lib

--- AngularGoogleClient/angularApp/vendor.ts
+++ AngularGoogleClient/angularApp/vendor.ts
@@ -2,8 +2,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import '../favicon.ico';
-
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
 
